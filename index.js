9f3ea2cb70f844e0eb81f8c3fdf8d03e2c323daf
Marcus Walker <mwalke35@yahoo.com>
2023-01-14 14:47:53 -0500

    added html start of some html, employee questions added, added some styling

--- index.js
+++ index.js
@@ -5,121 +5,105 @@ const Employee = require("./lib/Employee.js");
 const engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require('./lib/Manager');
-teamArray = [];
+const { Console } = require('console');
+const { name } = require('ejs');
+const { normalize } = require('path');
+const teamArray = [];
 
 
 
 
 const managerQuestions = [{
-        type: "input",
-        name: "manager",
-        message: "What is the Manager's Name?",
-    }, {
-        type: "input",
-        name: "id",
-        message: "What is the employee ID?",
-    }, {
-        type: "input",
-        name: "email",
-        message: "What is the email address?",
-    }, {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the Manager's office number?",
-    }]
+    type: "input",
+    name: "manager",
+    message: "What is the Manager's Name?",
+}, {
+    type: "input",
+    name: "id",
+    message: "What is the employee ID?",
+}, {
+    type: "input",
+    name: "email",
+    message: "What is the email address?",
+}, {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the Manager's office number?",
+}]
 
 
-
-    const engineerQuestions =
-    [{
-        type: "input",
-        name: "manager",
-        message: "What is the Engineer's Name?",
+const sharedQuestions = [
+    {
+        type: "list",
+        name: "menu",
+        message: "Choose a employee/intern to add or select Finsihed.",
+        choices: ["Engineer", "Intern", "Finished"],
     }, {
-        type: "input",
-        name: "id",
-        message: "What is the employee ID?",
-    }, {
-        type: "input",
-        name: "email",
-        message: "What is the email address?",
-    }, {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the Engineer's GitHub name?",
-    }]
-
-const internQuestions =
-    [{
         type: "input",
         name: "intern",
         message: "What is the Intern's Name?",
+        when: (answers) => answers.menu === "Intern"
+    }, {
+        type: "input",
+        name: "Engineer",
+        message: "What is the Engineer's Name?",
+        when: (answers) => answers.menu === "Engineer"
     }, {
         type: "input",
         name: "id",
-        message: "What is the employee ID?",
+        message: "What is the Intern's ID#?",
+        when: (answers) => answers.menu === "Intern",
+    }, {
+        type: "input",
+        name: "id",
+        message: "What is the Engineer's ID#?",
+        when: (answers) => answers.menu === "Engineer"
     }, {
         type: "input",
         name: "email",
         message: "What is the email address?",
-    }, {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the Intern's GitHub name?",
-    }]
-
-    const sharedQuestions = [
-        { 
-            type: "list",
-            name: "menu",
-            message: "Choose a employee to add or select Finsihed.",
-            choices: ["Engineer", "Intern", "Finished"],
-        },
-        {
-        type: "input",
-        name: "manager",
-        message: "What is the Employee's Name?",
-    }, {
-        type: "input",
-        name: "id",
-        message: "What is the ID?",
+        when: (answers) => answers.menu === "Intern",
     }, {
         type: "input",
         name: "email",
         message: "What is the email address?",
+        when: (answers) => answers.menu === "Engineer",
     }, {
         type: "input",
-        name: "officeNumber",
+        name: "GitHub",
         message: "What is the Engineer's GitHub name?",
         when: (answers) => answers.menu === "Engineer"
-    },{
+    }, {
         type: "input",
         name: "school",
         message: "What is the Interns's school name?",
         when: (answers) => answers.menu === "Intern"
-    }]
+    }
+]
 
- function askQuestions() {
+
+function askQuestions() {
     inquirer.prompt(sharedQuestions)
-    .then((userInput) => {
-        console.log(userInput)
-        if (userInput.menu !== "Finished") {
-            return askQuestions() 
-        } else {
-            
-        };
-       
-        
-    })  
- }
+        .then((userInput) => {
+            console.log(userInput)
+            if (userInput.menu !== "Finished") {
+                return askQuestions()
+
+            } else {
+                process.exit(0)
+            }
+        })
+}
 
 //Function call to call first manager
 function init() {
     inquirer.prompt(managerQuestions)
         .then((userInput) => {
+            teamArray.push(userInput)
             console.log(userInput)
-        askQuestions()
-        })   
+            console.log(teamArray)
+            askQuestions()
+        })
 }
 
 //Function call to initialize app
